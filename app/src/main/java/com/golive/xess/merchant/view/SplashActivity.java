fd5372de12e7191c02e0babe042c1fb33732a023
ycyangchun <rujishui@163.com>
2017-03-16 14:12:37 +0800

    main ic

--- app/src/main/java/com/golive/xess/merchant/view/SplashActivity.java
+++ app/src/main/java/com/golive/xess/merchant/view/SplashActivity.java
@@ -39,7 +39,7 @@ public class SplashActivity extends BaseActivity implements SplashContract.View 
                 .splashModule(new SplashModule(this))
                 .build().inject(this);
         presenter.updateDevice(this);
-
+        startActivity(new Intent(this, MainActivity.class));
     }
 
 
