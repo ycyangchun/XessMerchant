37982a8ddb7cd7fd145d22bf5b9c758cbab399c5
ycyangchun <rujishui@163.com>
2017-03-25 17:14:21 +0800

    dialog xml 适配2

--- app/src/main/java/com/golive/xess/merchant/XessConfig.java
+++ app/src/main/java/com/golive/xess/merchant/XessConfig.java
@@ -38,10 +38,11 @@ public class XessConfig {
      * 测试环境根路径
      */
     public static final String SERVER_ROOT_TEST = "http://211.99.241.7:8809/";
+//    public static final String SERVER_ROOT_TEST = "http://172.17.0.117:8090/GoLive-xess/";
     /**
      * 生产版本根路径
      */
-    public static final String SERVER_ROOT_PRODUCT = "http://static.owspace.com/";
+    public static final String SERVER_ROOT_PRODUCT = "";
 
     public static String getServerUrl() {
         StringBuilder serverUrl = new StringBuilder("");
