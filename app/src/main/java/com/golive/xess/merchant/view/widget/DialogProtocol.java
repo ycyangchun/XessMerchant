b3da978240e7c10c1f65177fd13f206fe62b793c
ycyangchun <rujishui@163.com>
2017-03-30 10:19:49 +0800

    用户协议

--- app/src/main/java/com/golive/xess/merchant/view/widget/DialogProtocol.java
+++ app/src/main/java/com/golive/xess/merchant/view/widget/DialogProtocol.java
@@ -2,7 +2,9 @@ package com.golive.xess.merchant.view.widget;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.golive.xess.merchant.R;
@@ -13,13 +15,15 @@ import butterknife.OnClick;
 
 public class DialogProtocol extends Dialog {
 
-    @BindView(R.id.protocol_tv)
-    TextView protocolTv;
+    @BindView(R.id.protocol_wv)
+    WebView wvProtocol;
     @BindView(R.id.dialog_title_tv)
     TextView dialogTitleTv;
 
+    private Context context;
     public DialogProtocol(Context context, String themeResId) {
         super(context, R.style.ShareDialog);
+        this.context = context;
     }
 
     @Override
@@ -27,8 +31,9 @@ public class DialogProtocol extends Dialog {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_protocol);
         ButterKnife.bind(this);
-        dialogTitleTv.setText("Xess mini 用户服务协议");
-        protocolTv.setText("");
+        dialogTitleTv.setText(context.getString(R.string.protocol_title));
+        wvProtocol.setBackgroundColor(Color.argb(0, 0, 0, 0));
+        wvProtocol.loadUrl("file:///android_asset/user_protocol.htm");
     }
 
     @OnClick(R.id.dialog_close_tv)
