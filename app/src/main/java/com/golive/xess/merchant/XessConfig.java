d9aa2d75ebd99e7f74df4dd47acaf50e497e1be2
ycyangchun <rujishui@163.com>
2017-04-05 09:15:42 +0800

    域名

--- app/src/main/java/com/golive/xess/merchant/XessConfig.java
+++ app/src/main/java/com/golive/xess/merchant/XessConfig.java
@@ -37,13 +37,13 @@ public class XessConfig {
     /**
      * 测试环境根路径
      */
-    public static final String SERVER_ROOT_TEST = "http://211.99.241.7:8809/";
+    public static final String SERVER_ROOT_TEST = "http://golive-xess.dev.lhq-golive.com";//http://211.99.241.7:8809/
 //    public static final String SERVER_ROOT_TEST = "http://172.17.2.67:8888/GoLive-xess/";
     /**
      * 生产版本根路径
      */
 
-    public static final String SERVER_ROOT_PRODUCT = "";
+    public static final String SERVER_ROOT_PRODUCT = "http://golive-xess.dev.lhq-golive.com";
 
     public static String getServerUrl() {
         StringBuilder serverUrl = new StringBuilder("");
